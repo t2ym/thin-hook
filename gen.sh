c5bb4a64bd5ea57dafc1453e384128f4fc625854
Tetsuya Mori <t2y3141592@gmail.com>
2017-01-27 14:33:08 +0900

    Add gulpfile.js

--- gen.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# gen.sh
-browserify hook.js --s hook -o hook.browserify.js 
-babel --presets es2015 hook.browserify.js -o hook.es5.js 
-uglifyjs hook.es5.js -o hook.min.js
-rm -f hook.browserify.js hook.es5.js
---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const gulp = require('gulp');
+const runSequence = require('run-sequence');
+const babel = require('gulp-babel');
+const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const uglify = require('gulp-uglify');
+const fs = require('fs');
+const through = require('through2');
+const path = require('path');
+
+const hook = require('./hook.js');
+
+gulp.task('examples', () => {
+  return gulp.src([ 'examples/**/*.js', '!examples/**/hooked.*' ])
+    //.pipe(sourcemaps.init())
+    .pipe(through.obj((file, enc, callback) => {
+      let code = String(file.contents);
+      let basename = path.basename(file.path);
+      let hooked = hook.preprocess(code, '__hook__', [ [ 'examples/' + basename, {} ] ], hook.methodContextGenerator);
+      file.contents = new Buffer(hooked);
+      callback(null, file);
+    }))
+    .pipe(rename((path) => { path.basename = 'hooked.' + path.basename; }))
+    //.pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./examples'));
+});
+
+gulp.task('build', () => {
+  return browserify('./hook.js', { standalone: 'hook' })
+    .bundle()
+    .pipe(source('hook.min.js'))
+    .pipe(buffer())
+    .pipe(babel({ "presets": [ 'es2015' ]}))
+    .pipe(uglify({ mangle: false }))
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('default', (done) => {
+  runSequence('build', 'examples', done);
+});
