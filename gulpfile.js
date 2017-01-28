44ba6cc54160e6da6ac500383ffb8c55f1a0ea8d
Tetsuya Mori <t2y3141592@gmail.com>
2017-01-28 12:35:34 +0900

    Fix #9. Use escodegen's compact option instead of babel and uglify

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,10 @@ const uglify = require('gulp-uglify');
 const fs = require('fs');
 const through = require('through2');
 const path = require('path');
+const chai = require('chai');
+const assert = chai.assert;
+const espree = require('espree');
+const escodegen = require('escodegen');
 
 const hook = require('./hook.js');
 
@@ -30,13 +34,60 @@ gulp.task('examples', () => {
     .pipe(gulp.dest('./examples'));
 });
 
+function _trimStartEndRaw(ast) {
+  if (ast && typeof ast === 'object') {
+    [ 'start', 'end', 'raw' ].forEach(prop => {
+      if (ast && ast.hasOwnProperty(prop)) {
+        delete ast[prop];
+      }
+    });
+  }
+  for (let target in ast) {
+    if (ast[target]) {
+      if (Array.isArray(ast[target])) {
+        for (let i = 0; i < ast[target].length; i++) {
+          let item = ast[target][i];
+          if (item && typeof item === 'object' && typeof item.type === 'string') {
+            _trimStartEndRaw(ast[target][i]);
+          }
+        }
+      }
+      else if (typeof ast[target] === 'object' && typeof ast[target].type === 'string') {
+        _trimStartEndRaw(ast[target]);
+      }
+    }
+  }
+}
+
 gulp.task('build', () => {
   return browserify('./hook.js', { standalone: 'hook' })
     .bundle()
     .pipe(source('hook.min.js'))
     .pipe(buffer())
-    .pipe(babel({ "presets": [ 'es2015' ]}))
-    .pipe(uglify({ mangle: true }))
+    //.pipe(babel({ "presets": [ 'es2015' ]}))
+    //.pipe(uglify({ mangle: true }))
+    .pipe(through.obj((file, end, callback) => {
+      // robust minification by escodegen's compact option
+      let code = String(file.contents);
+      let espreeOptions = { range: false, tokens: false, comment: false, ecmaVersion: 8 };
+      let originalAst = espree.parse(code, espreeOptions);
+      let minifiedCode = escodegen.generate(originalAst, { format: { compact: true } });
+      let minifiedAst = espree.parse(minifiedCode, espreeOptions);
+      _trimStartEndRaw(originalAst);
+      _trimStartEndRaw(minifiedAst);
+      let originalAstJson = JSON.stringify(originalAst, null, 2);
+      let minifiedAstJson = JSON.stringify(minifiedAst, null, 2);
+      try {
+        assert.equal(minifiedAstJson, originalAstJson, 'Minified AST is identical to the original AST');
+        file.contents = new Buffer(minifiedCode);
+      }
+      catch (e) {
+        fs.writeFileSync('_originalAst.json', originalAstJson);
+        fs.writeFileSync('_minifiedAst.json', minifiedAstJson);
+        throw e;
+      }
+      callback(null, file);
+    }))
     .pipe(gulp.dest('.'));
 });
 
