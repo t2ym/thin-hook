e820f84862cfa51b213c6eea88f0fbd4f5cb2563
Tetsuya Mori <t2y3141592@gmail.com>
2017-02-15 17:02:17 +0900

    [examples] Add examples for Fix #32

---
+++ examples/example4.js
@@ -0,0 +1,20 @@
+{
+  setTimeout(function X() {
+    console.log('setTimeout function')
+  }, 1000);
+  setTimeout(() => {
+    console.log('setTimeout arrow function')
+  }, 1000);
+  setTimeout('console.log("setTimeout string")', 1000);
+  let f1 = () => { console.log('setTimeout f1') };
+  setTimeout(f, 1000);
+  setInterval(function X() {
+    console.log('setInterval function')
+  }, 1000);
+  setInterval(() => {
+    console.log('setInterval arrow function')
+  }, 1000);
+  setInterval('console.log("setInterval string")', 1000);
+  let f2 = () => { console.log('setInterval f2') };
+  setInterval(f, 1000);
+}
---
+++ examples/hooked.example4.js
@@ -0,0 +1,44 @@
+{
+  setTimeout(function X() {
+    return __hook__(() => {
+      console.log('setTimeout function');
+    }, this, arguments, 'examples/example4.js,X');
+  }, 1000);
+  setTimeout((...args) =>
+    (__hook__(() => {
+      console.log('setTimeout arrow function');
+    }, this, args, 'examples/example4.js')), 1000);
+  hook.setTimeout('__hook__', [[
+      'examples/example4.js',
+      {}
+    ]], 'method')('console.log("setTimeout string")', 1000);
+  let f1 = (...args) =>
+    (__hook__(() => {
+      console.log('setTimeout f1');
+    }, this, args, 'examples/example4.js,f1'));
+  hook.setTimeout('__hook__', [[
+      'examples/example4.js',
+      {}
+    ]], 'method')(f, 1000);
+  setInterval(function X() {
+    return __hook__(() => {
+      console.log('setInterval function');
+    }, this, arguments, 'examples/example4.js,X');
+  }, 1000);
+  setInterval((...args) =>
+    (__hook__(() => {
+      console.log('setInterval arrow function');
+    }, this, args, 'examples/example4.js')), 1000);
+  hook.setInterval('__hook__', [[
+      'examples/example4.js',
+      {}
+    ]], 'method')('console.log("setInterval string")', 1000);
+  let f2 = (...args) =>
+    (__hook__(() => {
+      console.log('setInterval f2');
+    }, this, args, 'examples/example4.js,f2'));
+  hook.setInterval('__hook__', [[
+      'examples/example4.js',
+      {}
+    ]], 'method')(f, 1000);
+}
