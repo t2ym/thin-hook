abdae65a2d6b6f40e99e59bf305fceef5e0dc4b8
Tetsuya Mori <t2y3141592@gmail.com>
2017-02-19 16:46:55 +0900

    [demo] Update for Fix #42

--- demo/no-hook-authorization.js
+++ demo/no-hook-authorization.js
@@ -7,16 +7,16 @@
     //   hook.parameters.noHookAuthorizationPassed,
     //   hook.parameters.noHookAuthorizationFailed
     // JSONs are output to console in the learning mode
-    //'*': true, 
+    //'*': true,
     "35ae97a3305b863af7eb0ac75c8679233a2a7550e4c3046507fc9ea182c03615": true,
     "16afd3d5aa90cbd026eabcc4f09b1e4207a7042bc1e9be3b36d94415513683ed": true,
-    "8cbfdd73a981036d32071bd5d7c423bd364bc558663c910ea3413805b01780eb": true,
+    "ab92492bdd66e951c2f2407a03007204f4ff1087d62663573913910eb40f2de8": true,
     "ae11a06c0ddec9f5b75de82a40745d6d1f92aea1459e8680171c405a5497d1c8": true,
     "477a46cd19b0db0cc97d660a91cb57a8567e415d1e4a4efe4eafe6e0a6a2037b": true,
     "7ecb3aeb79e5b143f0f5efd4070e55584abf96ce4ad8b54f44dec377c2e455ee": true,
-    "a7cae24c3dd476f713903156b2e53549583bd7ef7e7702bdcce0cc47510bddfa": true,
-    "5431117b1217c2c7700295a776c5ff3df468925ae4e118b682532238dd1ef019": true,
-    "02c107ea633ed697acc12e1b3de1bcf2f0ef7cafe4f048e29553c224656ecd7a": true,
+    "011eb56201f405c5fd58dcd8b87d7d7386790bcea1ede0660dbe226e2155f0c3": true,
+    "5820d25b817638bf6a31ef28654b014590cedc989b02405c819c513e6493ce44": true,
+    "02c107ea633ed697acc12e1b3de1bcf2f0ef7cafe4f048e29553c224656ecd7a": true
   };
   const hidden = Symbol('hidden');
   const passcode = 'XX02c107ea633ed697acc12e1b3de1bcf2f0ef7cafe4f048e29553c224656ecd7a';
