53dc7ea928ce2744ca7a19759bf050c59f6b2048
Tetsuya Mori <t2y3141592@gmail.com>
2017-02-20 15:06:09 +0900

    [demo] Update with Fix #44 and refactoring for Issue #43

--- demo/no-hook-authorization.js
+++ demo/no-hook-authorization.js
@@ -2,6 +2,7 @@
   // Authorization Tickets for no-hook scripts
   // Ticket for this script itself is specified in URL of script tag as
   // hook.min.js?no-hook-authorization={ticket}
+  // Note: no-hook-authorization must not exist in learning mode
   let noHookAuthorization = {
     // '*' is for learning mode to detect authorization tickets in 
     //   hook.parameters.noHookAuthorizationPassed,
@@ -10,12 +11,11 @@
     //'*': true,
     "35ae97a3305b863af7eb0ac75c8679233a2a7550e4c3046507fc9ea182c03615": true,
     "16afd3d5aa90cbd026eabcc4f09b1e4207a7042bc1e9be3b36d94415513683ed": true,
-    "731f8d49901062c235c7dd1710faa4f07b8f9e19896df6e4c852d9985b02ed9e": true,
     "ae11a06c0ddec9f5b75de82a40745d6d1f92aea1459e8680171c405a5497d1c8": true,
     "477a46cd19b0db0cc97d660a91cb57a8567e415d1e4a4efe4eafe6e0a6a2037b": true,
     "7ecb3aeb79e5b143f0f5efd4070e55584abf96ce4ad8b54f44dec377c2e455ee": true,
-    "cbd570ba354d450d25524db962d1f0c14920c50ffe5f9ebd39f4259811fe2902": true,
-    "051f750b2923f568babbf4d3b67983e3a3ddce61026dd528d887367f3a71af97": true,
+    "195fa9207ae9646dd4b53da75ac72715c818cef3aa863e634bb12683e0a2e799": true,
+    "da2d80035870489294909a1f24524f2c3b328e8cba4967893bdbac10537c6d0d": true,
     "02c107ea633ed697acc12e1b3de1bcf2f0ef7cafe4f048e29553c224656ecd7a": true
   };
   const hidden = Symbol('hidden');
