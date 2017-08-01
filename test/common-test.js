7bd3488fb364f3dc5f9790c46959c1d0b7630b87
Tetsuya Mori <t2y3141592@gmail.com>
2017-08-01 10:55:39 +0900

    [test] Add comments on tricky handling of coverage data sets for test/hook.min.js

--- test/common-test.js
+++ test/common-test.js
@@ -3,7 +3,23 @@
 Copyright (c) 2017, Tetsuya Mori <t2y3141592@gmail.com>. All rights reserved.
 */
 {
+  /*
+    As the instrumented test/hook.min.js stores its coverage data into window.__coverage__,
+    WCT.share.__coverage__ has to be explicitly initialized by window.__coverage__.
+    This cannot be done in the instrumented code in test/hook.min.js since WCT.share object is reset
+    to an empty object {} on loading web-component-tester/browser.js and initializing WCT after test/hook.min.js is loaded.
+  */
   WCT.share.__coverage__ = window.__coverage__;
+  /*
+    Since hook.registerServiceWorker() in hook.min.js reloads the entry HTML page
+    to replace itself with its "decoded HTML" version with hook.min.js?service-worker-ready=true,
+    WCT's childRunner.share object, which has the value of WCT.share BEFORE the reloading of the page,
+    has to be updated with that from the "decoded HTML" page.
+    To ahieve thi, childRunner.loaded() handler for DOMContentLoaded event has to be
+    explicitly called again on loading the "decoded HTML" version.
+    No waiting for the "load" event is required as the coverage target is only hook.min.js, which has
+    already been loaded at the beginning of the page.
+  */
   let childRunner = WCT._ChildRunner.get(window);
   if (childRunner) {
     childRunner.loaded();
@@ -83,6 +99,10 @@ Copyright (c) 2017, Tetsuya Mori <t2y3141592@gmail.com>. All rights reserved.
           navigator.serviceWorker.controller.postMessage('coverage', [ channel.port2 ]);
         });
         if (this.swCoverage) {
+          /*
+            wct-istanbul 0.12.3 supports collection of multiple coverage data sets
+            in a single sub suite by setting WCT.share.__coverage__* as other coverage data
+          */
           WCT.share.__coverage__service_worker__ = this.swCoverage;
         }
       }
