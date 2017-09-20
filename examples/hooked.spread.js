db545528b01f61f0955499c76f13bf3d56e62f8c
Tetsuya Mori <t2y3141592@gmail.com>
2017-09-20 16:15:41 +0900

    [examples] Add enumeration check examples for Fix #108 and #109 Hook enumeration and in operator

--- examples/hooked.spread.js
+++ examples/hooked.spread.js
@@ -14,18 +14,24 @@ hook.global(__hook__, 'examples/spread.js', 'gX1', 'let')._p_gX1 = {
   ];
   let y = {
     c: 3,
-    ...hook.global(__hook__, 'examples/spread.js,y', 'gX1', 'get')._p_gX1
+    ...__hook__('*', hook.global(__hook__, 'examples/spread.js,y', 'gX1', 'get')._p_gX1, [], 'examples/spread.js,y')
   };
   let z = {
     a: 1,
-    ...{
+    ...__hook__('*', {
       a: 1,
       b: 2,
-      ...__hook__(function () {
+      ...__hook__('*', __hook__(function () {
         return __hook__(() => {
           return y;
         }, this, arguments, 'examples/spread.js,z');
-      }, this, [], 'examples/spread.js,z', 0)
-    }
+      }, this, [], 'examples/spread.js,z', 0), [], 'examples/spread.js,z')
+    }, [], 'examples/spread.js,z')
   };
+  __hook__('in', z, ['a'], 'examples/spread.js');
+  let p;
+  for (p in __hook__('*', z, [], 'examples/spread.js')) {
+  }
+  for (p of __hook__('*', a2, [], 'examples/spread.js')) {
+  }
 }
--- examples/spread.js
+++ examples/spread.js
@@ -4,4 +4,8 @@ let gX1 = { a: 1, b: 2 };
   let a2 = [ 3, 4, ...gA1 ];
   let y = { c: 3, ...gX1 };
   let z = { a: 1, ...{ a: 1, b: 2, ...(function () { return y; })() } };
+  'a' in z;
+  let p;
+  for (p in z) {}
+  for (p of a2) {}
 }
