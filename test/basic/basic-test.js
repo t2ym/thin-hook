89752c746be4f4a07d968f6440236a431db10e3c
Tetsuya Mori <t2y3141592@gmail.com>
2017-10-04 18:52:17 +0900

    [test] Issue #130. HookApiTest: ExpressionStatement (in progress)

--- test/basic/basic-test.js
+++ test/basic/basic-test.js
@@ -172,6 +172,10 @@ Copyright (c) 2017, Tetsuya Mori <t2y3141592@gmail.com>. All rights reserved.
             eval: 'call'
           },
         ],
+        ExpressionStatement: [
+          { code: '1;', hooked: '1;' },
+          { code: 'a;', hooked: 'a;', eval: 'throw', options: 'initialScope', customOptionParams: { initialScope: { a: true } } },
+        ],
         ArrayExpression: [
           { name: 'empty Array', code: `[]`, hooked: `[];` },
           { name: 'Array', code: `[1,'a',true]`, hooked: `[1,'a',true];` },
