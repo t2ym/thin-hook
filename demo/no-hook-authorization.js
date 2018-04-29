30fcc8fa396dbd2768d39bf7b99cd88c828d8a59
Tetsuya Mori <t2y3141592@gmail.com>
2018-04-30 03:04:56 +0900

    0.0.250 with Fix #254 Block direct source code access even after the app shutdown and browser restarts

--- demo/no-hook-authorization.js
+++ demo/no-hook-authorization.js
@@ -9,7 +9,7 @@
     //   hook.parameters.noHookAuthorizationFailed
     // JSONs are output to console in the learning mode
     //'*': true,
-    "b1e89819ce34bc8711ca80ba6b448fc9d1b04f2150108ef3da50a00b35e0abe8": true, // hook.min.js
+    "73be342eae90271783c2b18ba516f5244c906284858a70dc7710080c5389a0b9": true, // hook.min.js
     "db6a0db884e84741c53936bfa1163b9c950b81d6c3cb0d78efc507fce5935f30": true, // demo/disable-devtools.js
     "7e0fcbf73f8a30d98082c497e4bec73f2b49e5bee70605bb8838aed035763868": true, // demo/context-generator.js
     "936299cf27465a19054ab800609fe1723469d204b45f239172b2018e1970126b": true, // demo/bootstrap.js
