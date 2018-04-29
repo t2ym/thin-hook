3f59426864df8b3390be4f03d2c00df5802d8ee5
Tetsuya Mori <t2y3141592@gmail.com>
2018-04-30 02:09:31 +0900

    0.0.249 with Fix #254 Block direct access to source codes even after the app shutdown

--- demo/no-hook-authorization.js
+++ demo/no-hook-authorization.js
@@ -9,7 +9,7 @@
     //   hook.parameters.noHookAuthorizationFailed
     // JSONs are output to console in the learning mode
     //'*': true,
-    "056ea11bdaf63565dee14797c60aa785ae64cabbc1d713d5e4c72ea51ffa131f": true, // hook.min.js
+    "b1e89819ce34bc8711ca80ba6b448fc9d1b04f2150108ef3da50a00b35e0abe8": true, // hook.min.js
     "cfc42f1bbb689dee99817053c81187031833841951102a701e13fc99f7b9ccd6": true, // demo/disable-devtools.js
     "7e0fcbf73f8a30d98082c497e4bec73f2b49e5bee70605bb8838aed035763868": true, // demo/context-generator.js
     "936299cf27465a19054ab800609fe1723469d204b45f239172b2018e1970126b": true, // demo/bootstrap.js
