b930f11429219ef3f697a6f4cd8ad226fa14920d
Tetsuya Mori <t2y3141592@gmail.com>
2020-02-07 18:36:33 +0900

    [demo][my-view2] Rebuild with Fix #335 Skip enumerating __proto__ property to avoid the side effect of the changes in Issue #325

--- demo/no-hook-authorization.js
+++ demo/no-hook-authorization.js
@@ -33,7 +33,7 @@ else {
     "c135fd6ba3cad41e63985ecca191995bf311abc756c5f574ef5b641e7db56914": true, // (function writeln2() { console.log("no-hook script tag via document.writeln"); })()
     "e233738578fd7e8f2e961fb11885e2c187146314a8e3fc65692633ff89c5d34a": true, // (function writeln4() { console.log("no-hook script tag in div tag via document.writeln"); })()
     "4f0395d52a8c1c7edaacacade9c31fe18555b79ce963dfb1abaaa34990993374": true, // location = "about:blank";
-    "4cf2ce78390512eaa7801119d5392aaac0c60abea71227af4968c9960a4f81cf": true, // demo/integrity.js
+    "38dde93aab47d48784d5096b44962ec89a006d571bf8b5eb9f7675b1b55c897c": true, // demo/integrity.js
     "ba07654d1c24a271ba0fa7ee5c55cd60c02133b0e53cb6bb6e634da473b1e540": true, // demo/script-hashes.js
     "0242016e2887b322ab28b887f03779c63899db232bb036d794a0278db9e54339": true, // demo/content-loader.js
   };
