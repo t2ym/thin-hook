9e2ba88232fffb6b21a7150acefcff17d61a8d12
Tetsuya Mori <t2y3141592@gmail.com>
2020-09-03 17:27:03 +0900

    [demo] Rebuild with Fix #388 [cache-bundle] Insert some delay after fetching about-blank-redirector.js

--- demo-frontend/components/thin-hook/demo/cache-automation.js
+++ demo-frontend/components/thin-hook/demo/cache-automation.js
@@ -64,5 +64,6 @@ async function automationFunction() {
     const baseURL = new URL(location.href);
     await fetch(new URL('about-blank-redirector.html', baseURL));
     await fetch(new URL('about-blank-redirector.js?no-hook=true', baseURL));
+    await new Promise(_resolve => { setTimeout(_resolve, 2000); });
   })();
 }
