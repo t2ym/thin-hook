57f994fc52c730782b07b1d8538880b47d04eb3f
Tetsuya Mori <t2y3141592@gmail.com>
2020-10-13 22:14:52 +0900

    0.4.0-alpha.40 with Issue #386 [Enhancement][WIP] Add plugins for injection (frontend parts) and demo-raw/ as the injection starting point

--- demo/no-hook-authorization.js
+++ demo/no-hook-authorization.js
@@ -13,7 +13,7 @@ else {
     //   hook.parameters.noHookAuthorizationFailed
     // JSONs are output to console in the learning mode
     //'*': true,
-    "e303a29446794a5544eb6dd4b1d75b3ff7c97b99751a4a25454a0e4a69274bbb": true, // hook.min.js
+    "11ec57fcfa096df80e82e6bafd90b8ad94453998bfb9308321ee54f71366d56e": true, // hook.min.js
     "a81209228209941097ac41c5bda3bc1cd64375e69b7c8113aff68c2fe6729ff2": true, // demo/disable-devtools.js
     "4374ae1f28cec2b969ab488895a4266244d65e28e1f82728fd2cd39130fe0d9e": true, // demo/context-generator.js
     "e96184a4e08a67524b596ac298b8f61316e2f267cacdd678836b955e03764179": true, // demo/bootstrap.js
