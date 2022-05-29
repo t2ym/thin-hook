f7ecab74eacb7e30f9a0a80f2743d8e587a7f1d8
Tetsuya Mori <t2y3141592@gmail.com>
2022-05-29 20:48:08 +0900

    0.4.0-alpha.51 with Fix #422 [integrity-js] Skip navigation object on browserHash traversal

--- demo/no-hook-authorization.js
+++ demo/no-hook-authorization.js
@@ -13,7 +13,7 @@ else {
     //   hook.parameters.noHookAuthorizationFailed
     // JSONs are output to console in the learning mode
     //'*': true,
-    "b67e22a0bfe053cc76dfe23eb8367f83c9307149a1066ebf5341cb38b2e9bdfe": true, // hook.min.js
+    "1eac553da1abc22c5bb8f9c9df1ee3758a11f3c2c1573dec20028b369554dde7": true, // hook.min.js
     "a81209228209941097ac41c5bda3bc1cd64375e69b7c8113aff68c2fe6729ff2": true, // demo/disable-devtools.js
     "4374ae1f28cec2b969ab488895a4266244d65e28e1f82728fd2cd39130fe0d9e": true, // demo/context-generator.js
     "dca933fb4ade771ca8ed30823b52e7e36762522711ebf1d87eb9905c21b840ea": true, // demo/bootstrap.js
@@ -35,7 +35,7 @@ else {
     "c135fd6ba3cad41e63985ecca191995bf311abc756c5f574ef5b641e7db56914": true, // (function writeln2() { console.log("no-hook script tag via document.writeln"); })()
     "e233738578fd7e8f2e961fb11885e2c187146314a8e3fc65692633ff89c5d34a": true, // (function writeln4() { console.log("no-hook script tag in div tag via document.writeln"); })()
     "4f0395d52a8c1c7edaacacade9c31fe18555b79ce963dfb1abaaa34990993374": true, // location = "about:blank";
-    "e6405b7d43df7869e54952a9b00fa05a63d2c9206ad5853ab0e7c07265baedd0": true, // demo/integrity.js
+    "63f87adc3e346752a761c840f9aedf26ee6083e73c173ffacd125ba47f72e40b": true, // demo/integrity.js
     "ba07654d1c24a271ba0fa7ee5c55cd60c02133b0e53cb6bb6e634da473b1e540": true, // demo/script-hashes.js
     "0242016e2887b322ab28b887f03779c63899db232bb036d794a0278db9e54339": true, // demo/content-loader.js
     "6249373b8a4e3bb915788ea4f77e8b892f5bb77407e4fe5d981ebd118876be20": true, // demo/mark-parsed.js
