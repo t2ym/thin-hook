bf58dab03fbbaa20f49bdb56e4587bc145ffa13b
Tetsuya Mori <t2y3141592@gmail.com>
2023-07-26 23:38:00 +0900

    [bootstrap-js] Issue #432 In monitoring mode, reporter scripts can be loaded as no-hook

--- demo-config/bootstrap-js/parameters.js
+++ demo-config/bootstrap-js/parameters.js
@@ -1,1 +1,6 @@
 /* additional parameters in hook.parameters.name = value; ... */
+/* @ifdef enableMonitoring */
+hook.parameters.noHook = [
+  url => new URL(url).origin === '/* @echo reporterOrigin */',
+];
+/* @endif */
